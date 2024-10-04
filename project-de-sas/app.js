3adff1cfd138b53d5a5d07bf76cbc6a1e9d541d8
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-10-04 19:24:39 +0100

    added error case while insering the infos, check inputs that not empty

--- project-de-sas/app.js
+++ project-de-sas/app.js
@@ -1,11 +1,34 @@
 const handleAddReservation = () => {
-    let ID = [];
-    let chiffres = [
+    const reservations = [];
+    let reservation = {};
+    const chiffres = [
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', // Numbers
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', // Uppercase letters
-      ];
-
+    ];
+    
+    const nom = document.getElementById("nom_value").value;
+    const prenom = document.getElementById("prenom_value").value;
+    const age = document.getElementById("age_value").value;
+    const phone = document.getElementById("phone_value").value;
+    const date_res = document.getElementById("date_res_value").value;
+    const statut = document.getElementById("statut_value").value;
+    let ID = [];
     for(let i=0; i<6; i++){
         ID[i] = chiffres[Math.round(Math.random() * chiffres.length)];
     }
+    // verifier que tous les inputs sont remplir et ne pas vide
+    if(nom && prenom && age && phone && date_res && statut){
+        reservation.id = ID.join("");
+        reservation.nom = nom;
+        reservation.prenom = prenom;
+        reservation.age = age;
+        reservation.phone = phone;
+        reservation.dateReservation = date_res;
+        reservation.statut = statut;
+        console.log(reservation);
+    }
+    else{
+        document.querySelector(".error").style.display = "block";
+    }
+
 }
