051b08006f6ac30adf432cc753521ef8b4bfa790
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-10-07 00:27:26 +0100

    fix issue, clear inputs after any insertion

--- project-de-sas/app.js
+++ project-de-sas/app.js
@@ -7,46 +7,46 @@ const handleAddReservation = () => {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', // Uppercase letters
     ];
     
-    let nom = document.getElementById("nom_value").value;
-    let prenom = document.getElementById("prenom_value").value;
-    let age = document.getElementById("age_value").value;
-    let phone = document.getElementById("phone_value").value;
-    let date_res = document.getElementById("date_res_value").value;
-    let statut = document.getElementById("statut_value").value;
+    let nom = document.getElementById("nom_value");
+    let prenom = document.getElementById("prenom_value");
+    let age = document.getElementById("age_value");
+    let phone = document.getElementById("phone_value");
+    let date_res = document.getElementById("date_res_value");
+    let statut = document.getElementById("statut_value");
     let ID = [];
     for(let i=0; i<6; i++){
         ID[i] = chiffres[Math.round(Math.random() * chiffres.length)];
     }
     // verifier que tous les inputs sont remplir et ne pas vide
-    if(nom && prenom && age && phone && date_res && statut){
+    if(nom.value && prenom.value && age.value && phone.value && date_res.value && statut.value){
         const regexPhone = /\d{10}/;
         const regexAge = /\d{2,3}/;
         const regexDate = /\d{1,2}\/\d{1,2}\/\d{4}/;
         // verifier est ce que le numero de telephone matcher exactement dix chiffres
-        if(!regexPhone.test(phone)){
+        if(!regexPhone.test(phone.value)){
             document.querySelector(".error").style.display = "block";
             return;
         }
         // verifier est ce que l'age est valide entre deux ou trois nombres
-        if(!regexAge.test(age)){
+        if(!regexAge.test(age.value)){
             document.querySelector(".error").style.display = "block";
             return;
         }
         // verifier est ce que la date de reservation est valide
-        if(!regexDate.test(date_res)){
+        if(!regexDate.test(date_res.value)){
             document.querySelector(".error").style.display = "block";
             return;
         }
         reservation.id = ID.join("");
-        reservation.nom = nom;
-        reservation.prenom = prenom;
-        reservation.age = age;
-        reservation.phone = phone;
-        reservation.dateReservation = date_res;
-        reservation.statut = statut;
+        reservation.nom = nom.value;
+        reservation.prenom = prenom.value;
+        reservation.age = age.value;
+        reservation.phone = phone.value;
+        reservation.dateReservation = date_res.value;
+        reservation.statut = statut.value;
         document.querySelector(".error").style.display = "none";
         //! This line not working, I don't know why! I'll fix it later
-        nom = ""; prenom = ""; age = ""; phone = ""; date_res = ""; statut = "";
+        nom.value = ""; prenom.value = ""; age.value = ""; phone.value = ""; date_res.value = ""; statut.value = "";
         document.querySelector(".succes").style.display = "block";
     }
     else{
@@ -74,6 +74,6 @@ else{
 const handleAfficheReservations = () => {
     console.log(reservations);
     reservations.forEach((item) => {
-        console.log("uid: " + item.id + " prenom: " + item.prenom);
+        
     })
 }
